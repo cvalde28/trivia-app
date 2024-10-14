a3d78094083fad6dbaaf6f18b033066e3fe2a62d
Crystal Valdez <cvalde28@stu.southtexascollege.edu>
2024-10-13 21:13:08 -0500

    API call is working

--- src/components/BaseTitle.vue
+++ src/components/BaseTitle.vue
@@ -1,8 +1,17 @@
 <template>
-    <div class="flex items-center justify-center gap-4">
-        <h1 class="text-6x1 font-thin text-slate-700 uppercase">
-
-            <slot />
-        </h1>
-    </div>
+  <div class="flex items-center justify-center gap-4">
+    <h1 class="text-6xl font-thin text-slate-700 uppercase">
+      <slot />
+    </h1>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'BaseTitle',
+};
+</script>
+
+<style scoped>
+/* Your styles for BaseTitle */
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,20 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import BaseTitle from '@/components/ui/BaseTitle.vue';
+import useAPI from '@/composables/useAPI'
+
+const { categories, getCategories } = useApi()
+
+onMounted(async () => {
+  await getCategories()
+})
+
+
 </script>
 
 <template>
   <BaseTitle>TRIVIA APPLICATION</BaseTitle>
-</template>
+  <div v-for="category in categories" :key="category.id">
+    {{ category.name }}
+  </div>
+  </template>
